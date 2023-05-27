94f25aa39b19f1e086dccf10fbb0de9cd79e0502
ENTITY514 <entity514@mail.ru>
2023-05-27 13:27:04 +0600

    sidebar update

--- src/Pages/Dashboard/Components/InfoBoxes/infoBoxes.tsx
+++ src/Pages/Dashboard/Components/InfoBoxes/infoBoxes.tsx
@@ -5,7 +5,6 @@ export const InfoBoxes = () => {
     return (
         <div className={style.container}>
             <InfoBox label={'Active Service Providers'} value={'1000'} />
-            <InfoBox label={'Total space used'} value={'10 Gb'} />
             <InfoBox label={'Average latency'} value={'100 ms'} />
             <InfoBox label={'Blocks count'} value={'45000'} />
         </div>
--- src/Pages/Providers/Components/InfoBoxes/infoBoxes.tsx
+++ src/Pages/Providers/Components/InfoBoxes/infoBoxes.tsx
@@ -6,15 +6,11 @@ export const InfoBoxes = () => {
         <div className={style.container}>
             <div className={style.box_one}>
                 <InfoBox label={'Balance'} value={'1,999 BNB'} />
-                <InfoBox label={'Store price'} value={'1 BNB'} />
-            </div>
-            <div className={style.box_one}>
                 <InfoBox label={'Read price'} value={'1 BNB'} />
                 <InfoBox label={'Store price'} value={'1 BNB'} />
             </div>
             <div className={style.box_two}>
                 <InfoBox label={'Latency'} value={'1000 ms'} />
-                <InfoBox label={'Total space used'} value={'10 Gb'} />
                 <InfoBox label={'Uptime'} value={'10 h. 23 m.'} />
             </div>
 
